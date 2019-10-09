8a1c01716b72e99029810a405bcee267d2c943d7
liurui <liu.rui@oyohotels.cn>
2019-10-09 10:49:30 +0800

    add pageHelper

--- src/main/java/com/oyo/spring4/model/Country.java
+++ src/main/java/com/oyo/spring4/model/Country.java
@@ -2,12 +2,14 @@ package com.oyo.spring4.model;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @author liurui
  * @date 2019/10/8 15:45
  */
 @Data
-public class Country {
+public class Country implements Serializable {
     private Integer id;
     private String countryCode;
     private String countryName;
