26a32bff0703d3642369d53f62956f911348b99e
liurui <liu.rui@oyohotels.cn>
2019-10-21 11:51:52 +0800

    add

--- src/main/java/com/oyo/demo1016/Test4.java
+++ src/main/java/com/oyo/demo1016/Test4.java
@@ -1,8 +1,17 @@
 package com.oyo.demo1016;
 
 public class Test4 {
+    public  int a  ;
+
+    public void test() {
+        int b = 0;
+        System.out.println(b);
+    }
+
+
+
     public static void main(String[] args) {
-        String name = OTAEnum.MEI_TUAN.name();
-        System.out.println(name);
+        Test4 test = new Test4();
+        test.test();
     }
 }
