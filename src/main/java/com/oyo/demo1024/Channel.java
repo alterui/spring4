1b89df536c4cb215b5426c273fea3d5438490d3f
liurui <liu.rui@oyohotels.cn>
2019-10-29 19:06:08 +0800

    add easyExcel

---
+++ src/main/java/com/oyo/demo1024/Channel.java
@@ -0,0 +1,15 @@
+package com.oyo.demo1024;
+
+import com.oyo.spring4.pageHelp.PagedResponse;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author liurui
+ * @date 2019/10/28 18:41
+ */
+@Data
+public class Channel {
+   private List<String> putAway;
+}
--- src/main/java/com/oyo/demo1024/Test.java
+++ src/main/java/com/oyo/demo1024/Test.java
@@ -1,6 +1,7 @@
 package com.oyo.demo1024;
 
 import com.google.common.collect.Lists;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -8,8 +9,10 @@ import java.util.stream.Collectors;
 public class Test  {
     public static void main(String[] args) {
         List<String> putAway = Lists.newArrayList();
-        putAway.add("美团");
-        putAway.add("携程");
+
+
+        Channel channel = new Channel();
+        channel.setPutAway(putAway);
 
         List<String> all = Lists.newArrayList();
         all.add("美团");
@@ -17,10 +20,15 @@ public class Test  {
         all.add("艺龙");
         all.add("飞猪");
 
+        if (CollectionUtils.isEmpty(channel.getPutAway())) {
+           all.forEach(System.out::println);
 
-        List<String> c = all.stream().filter(e -> !putAway.contains(e)).collect(Collectors.toList());
+        } else {
+            List<String> c = all.stream().filter(e -> !channel.getPutAway().contains(e)).collect(Collectors.toList());
+            c.forEach(System.out::println);
+        }
 
-        c.forEach(System.out::println);
+
 
     }
 }
