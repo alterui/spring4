7c28398c187fc0e883f5794f60124277f62b4b7b
liurui <liu.rui@oyohotels.cn>
2019-11-15 18:16:43 +0800

    add filter

---
+++ src/main/java/com/oyo/demo1029/Test.java
@@ -0,0 +1,38 @@
+package com.oyo.demo1029;
+
+import com.google.common.collect.Maps;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author liurui
+ * @date 2019/11/14 19:51
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Map<Long, Long> map = Maps.newHashMap();
+
+
+        String a = "";
+        Long b =null;
+
+        map.put(1L, 1L);
+
+
+
+        map.containsKey("");
+
+
+
+
+
+
+    }
+
+    public  <T> String  enumMap (T list){
+        return null;
+    }
+}
