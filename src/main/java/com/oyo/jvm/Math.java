a84b7035963d4483676ff1d65c254410a443e53b
liurui <liu.rui@oyohotels.cn>
2019-12-09 19:20:01 +0800

    RedisService

--- src/main/java/com/oyo/jvm/Math.java
+++ src/main/java/com/oyo/jvm/Math.java
@@ -1,8 +1,28 @@
 package com.oyo.jvm;
 
+import com.oyo.demo.demo1029.Student;
+import org.apache.catalina.User;
+
 /**
  * @author liurui
  * @date 2019/12/9 15:00
  */
+
+
+
 public class Math {
+    public static int initData = 666;
+    public static Student user = new Student();
+    public int compute(){
+        int a =1;
+        int b = 2;
+        int c = (a+b)*10;
+        return c;
+    }
+
+    public static void main(String[] args) {
+        Math math = new Math();
+        math.compute();
+        System.out.println("test");
+    }
 }
