ebba5fbb85a1ea17a139a5c7d0a1cf95cf38d93d
liurui <liu.rui@oyohotels.cn>
2019-12-11 11:36:17 +0800

    RedisService

--- src/main/java/com/oyo/car/GetBMWCat.java
+++ src/main/java/com/oyo/car/GetBMWCat.java
@@ -3,6 +3,9 @@ package com.oyo.car;
 import com.oyo.exception.ServiceException;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author liurui
  * @date 2019/9/20 18:43
@@ -21,5 +24,8 @@ public class GetBMWCat implements GetCar {
             //throw ServiceException.builder().msg("json串不能为空").build();
             throw new ServiceException("json串不能为空","s");
         }
+
+        Map<Object, Object> map = new HashMap<>();
+        map.put(null, null);
     }
 }
