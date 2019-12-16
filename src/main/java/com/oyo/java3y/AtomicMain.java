175418a5adba88f880ec22a69fbcd37c2c747bcb
liurui <liu.rui@oyohotels.cn>
2019-12-16 17:38:31 +0800

    添加Atomic包，线程安全

--- src/main/java/com/oyo/java3y/AtomicMain.java
+++ src/main/java/com/oyo/java3y/AtomicMain.java
@@ -1,9 +1,15 @@
 package com.oyo.java3y;
 
+import com.alibaba.excel.util.DateUtils;
 import org.checkerframework.checker.units.qual.C;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -15,10 +21,24 @@ public class AtomicMain {
     public static void main(String[] args) throws InterruptedException {
         ExecutorService service = Executors.newCachedThreadPool();
 
+
+        String format = DateUtils.format(new Date(), DateUtils.DATE_FORMAT_19);
+        System.out.println(format);
         Count count = new Count();
         for (int i = 0; i < 100; i++) {
             service.execute(count::increase);
         }
+
+        Integer i   = null;
+
+
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("YYYY-MM-dd HH:mm:ss");
+
+
+
+
+
 
         service.shutdown();
         boolean flag = service.awaitTermination(1, TimeUnit.HOURS);
