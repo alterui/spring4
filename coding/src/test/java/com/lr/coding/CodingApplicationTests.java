0714494e2e80230281b3a2881cbc2af00f1423d0
alterui <656812771@qq.com>
2020-01-03 23:23:26 +0800

    add new branch and model

---
+++ coding/src/test/java/com/lr/coding/CodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lr.coding;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
