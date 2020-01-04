db855819a282ffe246ee6a9b5551568a16e84e13
alterui <656812771@qq.com>
2020-01-04 16:39:28 +0800

    thread update

--- coding/src/main/java/com/lr/coding/thread/MyRunnable.java
+++ coding/src/main/java/com/lr/coding/thread/MyRunnable.java
@@ -18,8 +18,8 @@ public class MyRunnable implements Runnable {
         Thread thread1 = new Thread(runnable);
         Thread thread2 = new Thread(runnable);
 
-        thread1.run();
-        thread2.run();
+        thread1.start();
+        thread2.start();
 
     }
 }
